72bf987552bc1cc0f6a7157c1deed1efd3e4c8fe
Seda <seda.tarverdyan200407@gmail.com>
2021-03-16 18:29:34 +0300

    'index.js'

---
+++ homework1.js
@@ -0,0 +1,10 @@
+//1 exercise
+// console.log('My name: Seda Tarverdyan')
+// alert('My First Homework')
+
+//Or
+// const firstName = 'Seda'
+// const lastName = 'Tarverdyan'
+// console.log(firstName + lastName)
+// alert('My First Homework')
+
---
+++ homework2.js
@@ -0,0 +1,21 @@
+//2 exercise
+// const urName = prompt('Whats ur name?')
+// const age = prompt('How old r u?')
+// const town = prompt('Where r u from?')
+// alert(`My name is ${urName}, im ${age} years old. I am from ${town}.`)
+
+//3 exercise
+// const urNumber = +prompt('Write number')
+// if( isNaN(urNumber) && usNumber > 8 && urNumber < 20 ){
+//       console.log(true)
+//  }  else {
+//       console.log(false)
+// }
+
+// if(urNumber>8 && urNumber<20){
+//     console.log(true)
+// }
+// else{
+//     console.log(false)
+// }
+
